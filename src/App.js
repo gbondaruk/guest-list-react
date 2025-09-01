4e75e7a230db436a9ce1c40030e810989ce28976
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-09-01 12:08:45 +0200

    Start creating HTML structure of guest list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+  //Base URL for API
+  const baseUrl = 'http://localhost:4000';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //Display guest list
+    <div>
+      <h1>My Guest List</h1>
+      <label>
+        First Name:
+        <input></input>
+      </label>
+
+      <label>
+        Last Name:
+        <input></input>
+      </label>
     </div>
   );
 }
